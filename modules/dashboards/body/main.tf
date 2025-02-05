ea0a3086057a8a0b30364b0488bd3cc045c0f656
Jacek Marmuszewski <jacek.marmuszewski@letsgodevops.pl>
2025-02-05 14:03:11 +0100

    Module to create the dashboard in CloudWatch

---
+++ modules/dashboards/body/main.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  type        = string
+  description = "Name of the dashboard"
+}
+
+variable "widgets" {
+  type        = list(string)
+  description = "List of widgets to display on the dashboard"
+}
+
+
+locals {
+  dashboard_body = {
+    widgets : var.widgets
+  }
+}
+
+resource "aws_cloudwatch_dashboard" "this" {
+  dashboard_name = var.name
+
+  dashboard_body = jsonencode(local.dashboard_body)
+}
+
